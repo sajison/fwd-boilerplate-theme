ad8695a35a96cd8c9a8d842f04b6e998d85ace8a
Ben Davidson <ben@bendavidson.me>
2018-09-20 10:56:35 -0400

    Added file imports

--- app/app.php
+++ app/app.php
@@ -7,20 +7,31 @@ require( get_stylesheet_directory( ) .  '/vendor/autoload.php' );
 /**
  * Helper functions
  */
+require 'helpers/get-partial.php';
+require 'helpers/get-svg.php';
+require 'helpers/picsum-img-url.php';
+require 'helpers/placeholder-img-url.php';
+
 
 /**
  * Theme setup and supports
  */
+require 'setup/acf-save-location.php';
+require 'setup/admin-menu-remover.php';
 require 'setup/asset-loader.php';
+require 'setup/image-sizes.php';
 require 'setup/theme-settings-page.php';
+require 'setup/theme-supports.php';
 
 /**
  * Theme menus
  */
+require 'menus/navigation-menus.php';
 
 /**
  * Theme sidebars
  */
+require 'sidebars/sidebars.php';
 
 /**
  * Theme widgets
