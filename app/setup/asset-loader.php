77b24b6464e85795c76f81a887d047b0be9ae39e
benjatron <ben@bendavidson.me>
2018-12-06 14:02:50 -0500

    Fixed asset reference

--- app/setup/asset-loader.php
+++ app/setup/asset-loader.php
@@ -3,9 +3,9 @@
 function fwd_asset_loader()
 {
   // Styles
-  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/assets/styles/dist/main.css', false, null);
+  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/resources/styles/dist/main.css', false, null);
 
   // Scripts
-  wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/scripts/dist/main.js', array('jquery'), null, true);
+  wp_enqueue_script('main', get_stylesheet_directory_uri() . '/resources/scripts/dist/main.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'fwd_asset_loader', 100);
